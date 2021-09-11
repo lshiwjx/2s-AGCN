b60255bb4b68a9a314cebcd2d4028cfa2c42a3ce
zlngan <zlngan@126.com>
2021-09-11 10:07:19 +0800

    my-gcn-91.75%

--- model/my_gcn.py
+++ model/my_gcn.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.conv import Conv2d 
-
+from data_gen.ntu_gendata import get_context, get_context_spa, get_context_tem
 
 def import_class(name):
     components = name.split('.')
@@ -16,17 +16,11 @@ class unit_gcn(nn.Module):
     def __init__(self, in_channels, out_channels, spa_kernel_size):
         super().__init__()
         spa_kernel_size = spa_kernel_size
-        self.conv_depth = nn.Conv2d(in_channels, in_channels, (5,5), padding=(2,2), groups=in_channels)
-        self.conv_point = nn.Conv2d(in_channels, out_channels*spa_kernel_size, (1,1))
-        self.bn1 = nn.BatchNorm2d(in_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels*spa_kernel_size)
-        self.relu = nn.ReLU()
+        self.conv = nn.Conv2d(in_channels, out_channels*spa_kernel_size, (1,1))
         
     def forward(self, x, A):
         kernel_size = A.size(0)
-        n, c, t, v = x.size()
-        x = self.relu(self.bn1(self.conv_depth(x)))
-        x = self.relu(self.bn2(self.conv_point(x)))
+        x = self.conv(x)
         n, kc, t, v = x.size()
         x = x.view(n, kernel_size, kc // kernel_size, t, v)
         x = torch.einsum('nkctv,kvw->nctw', (x, A))
@@ -83,6 +77,23 @@ class Model(nn.Module):
                         requires_grad=False)
         self.register_buffer('A', A)
         
+        # context
+        self.context_spa = get_context_spa()
+        self.context_spa = torch.from_numpy(self.context_spa)
+        self.context_tem = get_context_tem()
+        self.context_tem = torch.from_numpy(self.context_tem)
+        self.context_spa = self.context_spa.unsqueeze(0)
+        self.context_tem = self.context_tem.unsqueeze(0)
+        
+        # concat 
+        self.emb1 = nn.Conv2d(3, 32, (1, 1))
+        self.bn1 = nn.BatchNorm2d(32)
+        self.emb2 = nn.Conv2d(25, 32, (1, 1))
+        self.bn2 = nn.BatchNorm2d(32)
+        self.emb3 = nn.Conv2d(300, 32, (1, 1))
+        self.bn3 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU()
+                
         spatial_kernel_size = A.size(0)
         temporal_kernel_size = 9
         kernel_size = (temporal_kernel_size, spatial_kernel_size)
@@ -90,7 +101,7 @@ class Model(nn.Module):
         # kwargs0 = {k: v for k, v in kwargs.items() if k != 'dropout'}
         
         self.st_gcn_networks = nn.ModuleList((
-            TCN_GCN_unit(in_channels, 64, kernel_size, 1, residual=False, **kwargs),
+            TCN_GCN_unit(96, 64, kernel_size, 1, residual=False, **kwargs),
             TCN_GCN_unit(64, 64, kernel_size, 1, **kwargs),
             TCN_GCN_unit(64, 64, kernel_size, 1, **kwargs),
             TCN_GCN_unit(64, 64, kernel_size, 1, **kwargs),
@@ -125,6 +136,18 @@ class Model(nn.Module):
         x = x.permute(0, 1, 3, 4, 2).contiguous()
         x = x.view(N * M, C, T, V)
 
+        # context
+        y = self.context_spa
+        C1, T, V = y.size()[1:]
+        y = y.expand(N*M, C1, T, V).cuda().float()
+        z = self.context_tem
+        C2, T, V = z.size()[1:]
+        z = z.expand(N*M, C2, T, V).cuda().float()
+        
+        x = self.relu(self.bn1(self.emb1(x)))
+        y = self.relu(self.bn2(self.emb2(y)))
+        z = self.relu(self.bn3(self.emb3(z)))
+        x = torch.cat((x, y, z), dim=1)
         
         # forwad
         for st_gcn, importance in zip(self.st_gcn_networks, self.edge_importance):
