1cb442d113d0505c660bec944176e6309f2c73ef
zlngan <zlngan@126.com>
2021-09-13 22:11:02 +0800

    my-gcn-91.50%

--- graph/tools.py
+++ graph/tools.py
@@ -19,9 +19,21 @@ def normalize_digraph(A):  # 除以每列的和
     return AD
 
 
-def get_spatial_graph(num_node, self_link, inward, outward):
+def get_spatial_graph(num_node, self_link, inward, outward, all_link):
     I = edge2mat(self_link, num_node)
     In = normalize_digraph(edge2mat(inward, num_node))
     Out = normalize_digraph(edge2mat(outward, num_node))
-    A = np.stack((I, In, Out))
+    all = normalize_digraph(edge2mat(all_link, num_node))
+    A = np.stack((I, In, Out, all))
+    return A
+
+
+def get_newsep_graph(num_node, self_link, inward, outward, new_self_links, newup_links, newdown_links):
+    I = edge2mat(self_link, num_node)
+    In = normalize_digraph(edge2mat(inward, num_node))
+    Out = normalize_digraph(edge2mat(outward, num_node))
+    I_new = edge2mat(new_self_links, num_node)
+    up = normalize_digraph(edge2mat(newup_links, num_node))
+    down = normalize_digraph(edge2mat(newdown_links, num_node))
+    A = np.stack((I, In, Out, I_new, up, down))
     return A
