87045b007b251782345821021f74264c9c113867
zlngan <zlngan@126.com>
2021-09-17 17:33:16 +0800

    自己改的原版的st-gcn，这次精度只有90.36%

--- model/my_gcn.py
+++ model/my_gcn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn.modules.conv import Conv2d 
+import torch.nn.functional as F 
+
 
 def import_class(name):
     components = name.split('.')
@@ -10,18 +10,18 @@ def import_class(name):
         mod = getattr(mod, comp)
     return mod
     
-    
 class unit_gcn(nn.Module):
     def __init__(self, in_channels, out_channels, spa_kernel_size):
         super().__init__()
-        self.kernel_size = spa_kernel_size
-        self.conv = nn.Conv2d(in_channels, out_channels*spa_kernel_size, (1,1))
-        
+        spa_kernel_size = spa_kernel_size
+        self.conv = nn.Conv2d(in_channels, out_channels*spa_kernel_size, 1)
+    
     def forward(self, x, A):
-        assert A.size(0) == self.kernel_size
+        mat_kernel_size = A.shape[0]
+        n, c, t, v = x.size()
         x = self.conv(x)
         n, kc, t, v = x.size()
-        x = x.view(n, self.kernel_size, kc // self.kernel_size, t, v)
+        x = x.view(n, mat_kernel_size, kc // mat_kernel_size, t, v)
         x = torch.einsum('nkctv,kvw->nctw', (x, A))
         return x
 
@@ -34,12 +34,11 @@ class unit_tcn(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(kernel_size, 1), padding=(pad, 0),
                             stride=(stride, 1))
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.bn(self.conv(x))
         return x
-    
     
 class TCN_GCN_unit(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, residual=True):
@@ -48,7 +47,7 @@ class TCN_GCN_unit(nn.Module):
         spa_kernel_size = kernel_size[1]
         self.gcn = unit_gcn(in_channels, out_channels, spa_kernel_size)
         self.tcn = unit_tcn(out_channels, out_channels, tem_kernel_size, stride=stride)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         
         if not residual:
             self.residual = lambda x: 0
@@ -62,7 +61,6 @@ class TCN_GCN_unit(nn.Module):
         x = self.tcn(self.gcn(x, A)) + self.residual(x)
         return self.relu(x)
 
-
 class Model(nn.Module):
     def __init__(self, in_channels, num_class, graph, graph_args,
                  edge_importance_weighting, **kwargs):
@@ -75,7 +73,6 @@ class Model(nn.Module):
                         dtype=torch.float32,
                         requires_grad=False)
         self.register_buffer('A', A)
-        
         
         spatial_kernel_size = A.size(0)
         temporal_kernel_size = 9
@@ -95,20 +92,19 @@ class Model(nn.Module):
             TCN_GCN_unit(256, 256, kernel_size, 1, **kwargs),
             TCN_GCN_unit(256, 256, kernel_size, 1, **kwargs),
         ))
-        
+
         # initialize parameters for edge importance weighting
         if edge_importance_weighting:
             self.edge_importance = nn.ParameterList([
-                # nn.Parameter(torch.ones(self.A.shape))
-                nn.Parameter(torch.ones(A.size()))
+                nn.Parameter(torch.ones(self.A.shape))
                 for i in self.st_gcn_networks
             ])
         else:
             self.edge_importance = [1] * len(self.st_gcn_networks)
-        
+
         # fcn for prediction
         self.fcn = nn.Conv2d(256, num_class, kernel_size=1)
-        
+
     def forward(self, x):
         # data normalization
         N, C, T, V, M = x.size() # N:batch大小 C:三轴坐标 T:帧数 V：关节点数 M：最大人体数
@@ -118,17 +114,16 @@ class Model(nn.Module):
         x = x.view(N, M, V, C, T)
         x = x.permute(0, 1, 3, 4, 2).contiguous()
         x = x.view(N * M, C, T, V)
-        
+
         # forwad
-        for st_gcn, importance in zip(self.st_gcn_networks, self.edge_importance):
-            x = st_gcn(x, self.A * importance)
+        for stgcn, importance in zip(self.st_gcn_networks, self.edge_importance):
+            x = stgcn(x, self.A * importance)
 
         # global pooling
         x = F.avg_pool2d(x, x.size()[2:])
         x = x.view(N, M, -1, 1, 1).mean(dim=1)
 
         # prediction
-        
         x = self.fcn(x)
         x = x.view(x.size(0), -1)
 
